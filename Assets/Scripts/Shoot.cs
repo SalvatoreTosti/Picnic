ab9489a62ed8aa12ef3cd0d33d69ddfcc0eb408b
Salvatore Tosti <saltosti@gmail.com>
2016-04-25 16:49:40 -0700

    Created more complicated animation loop for pistol.
    The pistol now has a separate reload and 'empty' state which have both animation and game logic.

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,6 +6,7 @@ public class Shoot : MonoBehaviour {
 	private Animator animator;
 	public Light muzzleFlash;
 	public Transform muzzleLocation;
+	public int magazineCount = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +16,33 @@ public class Shoot : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
-			Debug.Log ("Fired");
-			animator.SetTrigger ("Fired");
+
+			if (magazineCount > 0) {
+				Fire();
+			} else {
+				Reload ();
+			}
+
 		}
+	}
+
+	private void Fire(){
+		Debug.Log ("Fired");
+		animator.SetTrigger ("Fired");
+		magazineCount--;
+		if (magazineCount == 0) {
+			animator.SetTrigger ("MagazineEmpty");
+		}
+	}
+
+	private void Reload(){
+		Debug.Log ("Reloading");
+		animator.SetTrigger ("Reload");
+	}
+
+	public void ReloadAdjust(){
+		magazineCount = 10;
+		Debug.Log ("Reloaded");
 	}
 
 	public void Flash(){
